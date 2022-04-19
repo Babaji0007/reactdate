e21f542c3bcf8c586c0a201714058d6190bed0ca
Babaji0007 <95861900+Babaji0007@users.noreply.github.com>
2022-04-19 21:46:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Example } from './Components/Example';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='hii'> React Date Picker </h1>
+      <Example/>
+    </div>
+  );
+}
+
+export default App;
